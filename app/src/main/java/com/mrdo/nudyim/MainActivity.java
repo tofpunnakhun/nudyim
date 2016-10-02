64a62eb5b9a8cf119818b35ad0be7c24464ebb45
Chaiwat <onepiecepura@outlook.com>
2016-10-03 06:55:21 +0700

    Login with google

--- app/src/main/java/com/mrdo/nudyim/MainActivity.java
+++ app/src/main/java/com/mrdo/nudyim/MainActivity.java
@@ -1,13 +1,42 @@
 package com.mrdo.nudyim;
 
+import android.content.Intent;
+import android.content.pm.Signature;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
+
+    // Information google
+    private String mUsername;
+    private String mPhotoUrl;
+
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseUser mFirebaseUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Initialize Firebase Auth
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mFirebaseUser = mFirebaseAuth.getCurrentUser();
+
+        // Check user login with google
+        if (mFirebaseUser != null){
+            mUsername = mFirebaseUser.getDisplayName();
+            // Check photo
+            if (mFirebaseUser.getPhotoUrl() != null){
+                mPhotoUrl = mFirebaseUser.getPhotoUrl().toString();
+            }
+        }else{
+            startActivity(new Intent(this, SignInActivity.class));
+            finish();
+            return;
+        }
     }
 }
