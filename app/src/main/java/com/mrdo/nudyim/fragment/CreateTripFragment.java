0f868d63dce28af124569333bca24549e1a36599
Chaiwat <onepiecepura@outlook.com>
2016-10-07 07:32:45 +0700

    Create DatePickerTime

--- app/src/main/java/com/mrdo/nudyim/fragment/CreateTripFragment.java
+++ app/src/main/java/com/mrdo/nudyim/fragment/CreateTripFragment.java
@@ -3,21 +3,31 @@ package com.mrdo.nudyim.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.mrdo.nudyim.R;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * Created by onepi on 10/6/2016.
  */
 
 public class CreateTripFragment extends Fragment {
+
+    private static final int REQUEST_DATE = 9000;
+    private static final String DIALOG_DATE = "DIALOG_DATE";
+    private TextView mStartDate;
+    private TextView mEndDate;
 
     public static CreateTripFragment newInstance() {
         Bundle args = new Bundle();
@@ -36,8 +46,40 @@ public class CreateTripFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
         View rootView = inflater.inflate(R.layout.fragment_create_trip, container, false);
+
+        mStartDate = (TextView) rootView.findViewById(R.id.start_date_view);
+        mStartDate.setText(toShortDate(new Date()));
+        mStartDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fm = getFragmentManager();
+                DatePickerFragment datePickerFragment =
+                        DatePickerFragment.newInstance(new Date());
+                datePickerFragment.setTargetFragment(CreateTripFragment.this, REQUEST_DATE);
+                datePickerFragment.show(fm, DIALOG_DATE);
+            }
+        });
+
+        mEndDate = (TextView) rootView.findViewById(R.id.end_date_view);
+        mEndDate.setText(toShortDate(new Date()));
+        mEndDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fm = getFragmentManager();
+                DatePickerFragment datePickerFragment =
+                        DatePickerFragment.newInstance(new Date());
+                datePickerFragment.setTargetFragment(CreateTripFragment.this, REQUEST_DATE);
+                datePickerFragment.show(fm, DIALOG_DATE);
+            }
+        });
+
         return rootView;
     }
+
+    public static String toShortDate(Date date) {
+        return new SimpleDateFormat("MMM d, yyyy").format(date);
+    }
+
 
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
---
+++ app/src/main/java/com/mrdo/nudyim/fragment/DatePickerFragment.java
@@ -0,0 +1,72 @@
+package com.mrdo.nudyim.fragment;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.DatePicker;
+
+import com.mrdo.nudyim.R;
+
+import java.util.Calendar;
+import java.util.Date;
+
+/**
+ * Created by onepi on 10/7/2016.
+ */
+
+public class DatePickerFragment extends DialogFragment
+implements DialogInterface.OnClickListener{
+
+    protected static final String ARGUMENT_DATE = "ARG_DATE";
+
+    private Calendar mCalendar;
+    private DatePicker mDatePicker;
+
+    public static DatePickerFragment newInstance(Date date) {
+        Bundle args = new Bundle();
+        DatePickerFragment fragment = new DatePickerFragment();
+        args.putSerializable(ARGUMENT_DATE, date);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+        Date date = (Date) getArguments().getSerializable(ARGUMENT_DATE);
+
+        mCalendar = Calendar.getInstance();
+        mCalendar.setTime(date);
+        int year = mCalendar.get(Calendar.YEAR);
+        int month = mCalendar.get(Calendar.MONTH);
+        int day = mCalendar.get(Calendar.DAY_OF_MONTH);
+
+        View v = LayoutInflater.from(getActivity())
+                .inflate(R.layout.dialog_date, null);
+
+        mDatePicker = (DatePicker) v.findViewById(R.id.date_picker);
+        mDatePicker.init(year, month, day, null);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setView(v);
+//        builder.setTitle(R.string.date_picker_title);
+        builder.setPositiveButton(android.R.string.ok, this);
+
+        return builder.create();
+    }
+
+    @Override
+    public void onClick(DialogInterface dialog, int which) {
+//        int day = mDatePicker.getDayOfMonth();
+//        int month = mDatePicker.getMonth();
+//        int year = mDatePicker.getYear();
+//
+//        mCalendar.set();
+    }
+}
