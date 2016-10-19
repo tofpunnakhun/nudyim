564a7c54c17a1a3df4e251e735f00b3779dcd639
Chaiwat <onepiecepura@outlook.com>
2016-10-19 08:52:04 +0700

    Create notification

---
+++ app/src/main/java/com/ayp/nudyim/notification/FcmMessagingService.java
@@ -0,0 +1,83 @@
+package com.ayp.nudyim.notification;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.media.RingtoneManager;
+import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
+import com.ayp.nudyim.MainActivitySingle;
+import com.ayp.nudyim.R;
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.Map;
+
+/**
+ * Created by onepi on 10/19/2016.
+ */
+
+public class FcmMessagingService extends FirebaseMessagingService{
+     private static final String TAG = "FcmMessagingService";
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+        Log.d(TAG, "Already Message Received!");
+        RemoteMessage.Notification notification = remoteMessage.getNotification();
+        Map<String, String> data = remoteMessage.getData();
+        Log.e("FROM", remoteMessage.getFrom());
+        sendNotification(notification, data);
+    }
+
+    private void sendNotification(RemoteMessage.Notification notification, Map<String, String> data) {
+        Bitmap icon = BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher);
+
+        Intent intent = new Intent(this, MainActivitySingle.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+        Bundle bundle = new Bundle();
+        bundle.putString("picture_url", data.get("picture_url"));
+        intent.putExtras(bundle);
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT);
+
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+                .setContentTitle(notification.getTitle())
+                .setContentText(notification.getBody())
+                .setAutoCancel(true)
+                .setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION))
+                //.setSound(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.win))
+                .setContentIntent(pendingIntent)
+                .setContentInfo("Your Trip sir!")
+                .setLargeIcon(icon)
+                .setColor(Color.YELLOW)
+                .setLights(Color.RED, 1000, 300)
+                .setDefaults(Notification.DEFAULT_VIBRATE)
+                .setSmallIcon(R.mipmap.ic_launcher);
+
+        try {
+            String picture_url = data.get("picture_url");
+            if (picture_url != null && !"".equals(picture_url)) {
+                URL url = new URL(picture_url);
+                Bitmap bigPicture = BitmapFactory.decodeStream(url.openConnection().getInputStream());
+                notificationBuilder.setStyle(
+                        new NotificationCompat.BigPictureStyle().bigPicture(bigPicture).setSummaryText(notification.getBody())
+                );
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(0, notificationBuilder.build());
+    }
+}
--- app/src/main/java/com/ayp/nudyim/trip/CreateTripActivity.java
+++ app/src/main/java/com/ayp/nudyim/trip/CreateTripActivity.java
@@ -2,6 +2,8 @@ package com.ayp.nudyim.trip;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
@@ -13,16 +15,27 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 import com.ayp.nudyim.R;
+import com.ayp.nudyim.model.Trip;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.ayp.nudyim.model.Trip;
+import com.google.firebase.iid.FirebaseInstanceId;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 /**
  * Created by onepi on 10/6/2016.
@@ -32,6 +45,8 @@ public class CreateTripActivity extends AppCompatActivity implements DatePickerF
 
     private static final int REQUEST_START_DATE = 9000;
     private static final int REQUEST_END_DATE = 9009;
+
+    private static final String AUTH_KEY = "key=AIzaSyBZTgjZdeldRh5RoAjnx8o2pa2Qk23fU7U";
 
     private static final String DIALOG_DATE = "DIALOG_DATE";
     private static final int REQUEST_CODE = 1189;
@@ -159,6 +174,10 @@ public class CreateTripActivity extends AppCompatActivity implements DatePickerF
                 String key = mDatabaseReference.child("trip").push().getKey();
                 mDatabaseReference.child("trip").child(key).setValue(trip);
                 inviteFriendToDb(key);
+
+                // notification
+                sendWithOtherThread("token");
+
                 finish();
 //                getFragmentManager().popBackStack();
             default:
@@ -175,6 +194,78 @@ public class CreateTripActivity extends AppCompatActivity implements DatePickerF
                     .child(mFriendLists.get(i))
                     .setValue(true);
         }
+    }
+
+    private void sendWithOtherThread(final String typeOfToken) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                pushNotification(typeOfToken);
+            }
+        }).start();
+    }
+
+    private void pushNotification(String typeOfToken) {
+        Log.d(TAG, "Token: " + FirebaseInstanceId.getInstance().getToken());
+        JSONObject jPayload = new JSONObject();
+        JSONObject jNotification = new JSONObject();
+        JSONObject jData = new JSONObject();
+
+        try {
+            jNotification.put("title", "Hey It's me, Mr.Do");
+            jNotification.put("body", "I was just create trip, Can you see that?");
+            jNotification.put("sound", "default");
+            jNotification.put("badge", "1");
+//            jNotification.put("click_action", "OPEN_ACTIVITY");
+
+            jData.put("picture_url", "https://fbi.dek-d.com/27/0378/7611/118707044");
+
+            switch (typeOfToken){
+                case "tokens":
+                    JSONArray ja = new JSONArray();
+                    ja.put("c5pBXXsuCN0:APA91bH8nLMt084KpzMrmSWRS2SnKZudyNjtFVxLRG7VFEFk_RgOm-Q5EQr_oOcLbVcCjFH6vIXIyWhST1jdhR8WMatujccY5uy1TE0hkppW_TSnSBiUsH_tRReutEgsmIMmq8fexTmL");
+                    ja.put(FirebaseInstanceId.getInstance().getToken());
+                    jPayload.put("registration_ids", ja);
+                    break;
+                default:
+                    jPayload.put("to", FirebaseInstanceId.getInstance().getToken());
+            }
+
+            jPayload.put("priority", "high");
+            jPayload.put("notification", jNotification);
+            jPayload.put("data", jData);
+
+            URL url = new URL("https://fcm.googleapis.com/fcm/send");
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Authorization", AUTH_KEY);
+            conn.setRequestProperty("Content-Type", "application/json");
+            conn.setDoInput(true);
+
+            // send FCM message content.
+            OutputStream outputStream = conn.getOutputStream();
+            outputStream.write(jPayload.toString().getBytes());
+
+            InputStream inputStream = conn.getInputStream();
+            final String resp = convertStreamToString(inputStream);
+
+            Handler handler = new Handler(Looper.getMainLooper());
+            handler.post(new Runnable() {
+                @Override
+                public void run() {
+                    Log.d(TAG, ""+resp);
+                }
+            });
+
+
+        } catch (JSONException | IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String convertStreamToString(InputStream is) {
+        Scanner s = new Scanner(is).useDelimiter("\\A");
+        return s.hasNext() ? s.next().replace(",", ",\n") : "";
     }
 
     private Trip bindTrip() {
